ae63ed90275c46cad5c02fe423ed472643edc33c
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-07-04 22:32:37 +0700

    done skills section

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function Backend() {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">NodeJS</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Express</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">NestJS</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Firebase</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Backend;
