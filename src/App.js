1df6510e96f44fabdecf1bf67736fc0a87bacbf4
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-07-04 20:00:30 +0700

    css app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import About from "./components/about/About";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <Home />
+        <About />
+      </main>
     </>
   );
 }
