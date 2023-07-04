599e5cc79947da2fc0a0abc6831cc0c1d3d898ac
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-07-04 20:01:02 +0700

    create header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 function Header() {
+  // Toggle Menu
+  const [Toggle, showMenu] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
@@ -9,10 +11,10 @@ function Header() {
           Tung Nguyen
         </a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
+              <a href="#home" className="nav__link active-link">
                 <i className="uil uil-estate nav__icon"></i>
                 Home
               </a>
@@ -25,13 +27,13 @@ function Header() {
             </li>
 
             <li className="nav__item">
-              <a href="#skilsl" className="nav__link">
+              <a href="#skilsl" className="nav__link ">
                 <i className="uil uil-file-alt nav__icon"></i>Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#services" className="nav__link">
+              <a href="#services" className="nav__link ">
                 <i className="uil uil-briefcase-alt nav__icon"></i>Services
               </a>
             </li>
@@ -49,9 +51,11 @@ function Header() {
               </a>
             </li>
           </ul>
-          <i className="uil uil-times close"></i>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}></i>
         </div>
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
       </nav>
